f9d7d764b9c1be43159b4e4c7d0ef6ba97a41f4c
Nick Powell and Trevor Fast <nick@>
2017-03-29 11:21:00 -0700

    split alphabet into array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(function(){
-
+  var inputText = $("input#textbox").val();
+  var alphaSplit = []
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var alphaSplit = alphabet.split("");
+  console.log(alphaSplit);
 
 });
+
+
+
+//
+// for (char c = 'a'; c <= 'z'; c++) {
+//     alphabet[c - 'a'] = c;
+// }
