4f330d40566bff2ae475fa4764d8a34e207e8bc3
Nick Powell and Trevor Fast <trevor@>
2017-03-29 16:30:49 -0700

    add vowel location index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
 $(function(){
-  var inputText = $("input#textbox").val();
-  var alphaSplit = []
+  var alphaSplit = [];
   var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   var alphaSplit = alphabet.split("");
+  var vowelSplit = [];
+  var vowels = "AEIOUaeiouYy";
+  var vowelSplit = vowels.split("");
+  var newWord = "";
+
+  var allVowels = []
 
   $("form#dialect").submit(function(event) {
     event.preventDefault();
-    if (inputText.length === 1) {
-      return true
-    }  else {
-      return false }
-      $("#result").text("result");
 
-    });
+    var inputText = $("input#textbox").val();
+    if (inputText.length === 1) {
+      var result = inputText + "ay";
+    } else {
+      var result = "false"
+    }
+
+    for (var i = 0; i < inputText.length; i++) {
+      if(vowelSplit.indexOf(inputText.charAt(i)) !== -1) {
+        allVowels.push(i);
+        alert(allVowels);
+
+    //   }if (allVowels.charAt(0)) {
+    //     inputText.append(inputText.charAt(0));
+    //     inputText.shift(inputText.charAt(0));
+      }
+    }
+
+
+
+    $("#result").text(result);
+
 
 
   });
-
-
-
-
-
-
-
-//
-// for (char c = 'a'; c <= 'z'; c++) {
-//     alphabet[c - 'a'] = c;
-// }
+});
